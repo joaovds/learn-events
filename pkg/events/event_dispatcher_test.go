a3c935046aa2ca40978c28ee85627040a4515d82
João Victor da Silva <jv782063@gmail.com>
2024-02-06 11:26:04 -0300

    run gofmt

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -10,158 +10,157 @@ import (
 )
 
 type TestEvent struct {
-  Name    string
-  Payload interface{}
+	Name    string
+	Payload interface{}
 }
 
 func (e *TestEvent) GetName() string {
-  return e.Name
+	return e.Name
 }
 
 func (e *TestEvent) GetPayload() interface{} {
-  return e.Payload
+	return e.Payload
 }
 
 func (e *TestEvent) GetDateTime() time.Time {
-  return time.Now()
+	return time.Now()
 }
 
 type TestEventHandler struct {
-  ID int
+	ID int
 }
 
 func (h *TestEventHandler) Handle(event EventInterface) {
-  // Do nothing
+	// Do nothing
 }
 
 type EventDispatcherTestSuite struct {
-  suite.Suite
-  event TestEvent
-  event2 TestEvent
-  handler TestEventHandler
-  handler2 TestEventHandler
-  handler3 TestEventHandler
-  eventDispatcher *EventDispatcher
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
 }
 
 func (s *EventDispatcherTestSuite) SetupTest() {
-  s.event = TestEvent{Name: "test", Payload: "test"}
-  s.event2 = TestEvent{Name: "test2", Payload: "test2"}
-  s.handler = TestEventHandler{ID: 1}
-  s.handler2 = TestEventHandler{ID: 2}
-  s.handler3 = TestEventHandler{ID: 3}
-  s.eventDispatcher = NewEventDispatcher()
+	s.event = TestEvent{Name: "test", Payload: "test"}
+	s.event2 = TestEvent{Name: "test2", Payload: "test2"}
+	s.handler = TestEventHandler{ID: 1}
+	s.handler2 = TestEventHandler{ID: 2}
+	s.handler3 = TestEventHandler{ID: 3}
+	s.eventDispatcher = NewEventDispatcher()
 }
 
 // REGISTER METHOD
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Register() {
-  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
-  s.Nil(err)
-  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
-
-  assert.Equal(s.T(), &s.handler, s.eventDispatcher.handlers[s.event.GetName()][0])
-  assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][1])
+	assert.Equal(s.T(), &s.handler, s.eventDispatcher.handlers[s.event.GetName()][0])
+	assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][1])
 }
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
-  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Equal(ErrHandlerAlreadyRegistered, err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Equal(ErrHandlerAlreadyRegistered, err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 }
 
 // CLEAR METHOD
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
-  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
-  s.Nil(err)
-  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event2.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event2.GetName()]))
+	err = s.eventDispatcher.Register(s.event2.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event2.GetName()]))
 
-  s.eventDispatcher.Clear()
-  s.Equal(0, len(s.eventDispatcher.handlers))
+	s.eventDispatcher.Clear()
+	s.Equal(0, len(s.eventDispatcher.handlers))
 }
 
 // HAS METHOD
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Has() {
-  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
-  s.Nil(err)
-  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler))
-  assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler2))
-  assert.False(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler3))
+	assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler))
+	assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler2))
+	assert.False(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler3))
 }
 
 // DISPATCH METHOD
 
 type MockHandler struct {
-  mock.Mock
+	mock.Mock
 }
 
 func (m *MockHandler) Handle(event EventInterface) {
-  m.Called(event)
+	m.Called(event)
 }
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Dispatch() {
-  eh := &MockHandler{}
-  eh.On("Handle", &s.event)
-  s.eventDispatcher.Register(s.event.GetName(), eh)
-  s.eventDispatcher.Dispatch(&s.event)
-  
-  eh.AssertExpectations(s.T())
-  eh.AssertCalled(s.T(), "Handle", &s.event)
-  eh.AssertNumberOfCalls(s.T(), "Handle", 1)
+	eh := &MockHandler{}
+	eh.On("Handle", &s.event)
+	s.eventDispatcher.Register(s.event.GetName(), eh)
+	s.eventDispatcher.Dispatch(&s.event)
+
+	eh.AssertExpectations(s.T())
+	eh.AssertCalled(s.T(), "Handle", &s.event)
+	eh.AssertNumberOfCalls(s.T(), "Handle", 1)
 }
 
 // REMOVE METHOD
 
 func (s *EventDispatcherTestSuite) TestEventDispatcher_Remove() {
-  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
-  s.Nil(err)
-  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Remove(s.event.GetName(), &s.handler)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Remove(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler3)
-  s.Nil(err)
-  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler3)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  err = s.eventDispatcher.Remove(s.event.GetName(), &s.handler3)
-  s.Nil(err)
-  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	err = s.eventDispatcher.Remove(s.event.GetName(), &s.handler3)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
 
-  assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][0])
+	assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][0])
 }
 
 // SUIT RUN
 
 func TestSuite(t *testing.T) {
-  suite.Run(t, new(EventDispatcherTestSuite))
+	suite.Run(t, new(EventDispatcherTestSuite))
 }
--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -3,19 +3,19 @@ package events
 import "time"
 
 type EventInterface interface {
-  GetName() string
-  GetPayload() interface{}
-  GetDateTime() time.Time
+	GetName() string
+	GetPayload() interface{}
+	GetDateTime() time.Time
 }
 
 type EventHandlerInterface interface {
-  Handle(event EventInterface)
+	Handle(event EventInterface)
 }
 
 type EventDispatcherInterface interface {
-  Register(eventName string, handler EventHandlerInterface) error
-  Dispatch(event EventInterface) error
-  Remove(eventName string, handler EventHandlerInterface) error
-  Has(eventName string, handler EventHandlerInterface) bool
-  Clear() error
+	Register(eventName string, handler EventHandlerInterface) error
+	Dispatch(event EventInterface) error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear() error
 }
