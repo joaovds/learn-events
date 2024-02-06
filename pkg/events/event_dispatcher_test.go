7c640c7693c14d4322125a858049a55f3ae1df18
João Victor da Silva <jv782063@gmail.com>
2024-02-06 10:52:34 -0300

    tests and implements dispatcher has method

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -39,6 +39,7 @@ type EventDispatcherTestSuite struct {
   event2 TestEvent
   handler TestEventHandler
   handler2 TestEventHandler
+  handler3 TestEventHandler
   eventDispatcher *EventDispatcher
 }
 
@@ -47,6 +48,7 @@ func (s *EventDispatcherTestSuite) SetupTest() {
   s.event2 = TestEvent{Name: "test2", Payload: "test2"}
   s.handler = TestEventHandler{ID: 1}
   s.handler2 = TestEventHandler{ID: 2}
+  s.handler3 = TestEventHandler{ID: 3}
   s.eventDispatcher = NewEventDispatcher()
 }
 
@@ -94,6 +96,24 @@ func (s *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
   s.eventDispatcher.Clear()
   s.Equal(0, len(s.eventDispatcher.handlers))
 }
+
+// HAS METHOD
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Has() {
+  err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+  s.Nil(err)
+  s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+  err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+  s.Nil(err)
+  s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+  assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler))
+  assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler2))
+  assert.False(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler3))
+}
+
+// SUIT RUN
 
 func TestSuite(t *testing.T) {
   suite.Run(t, new(EventDispatcherTestSuite))
