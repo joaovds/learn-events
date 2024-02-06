e21a0906567768baa3abff37564a29c559c86b86
João Victor da Silva <jv782063@gmail.com>
2024-02-06 16:59:35 -0300

    setup http serve

--- cmd/events/main.go
+++ cmd/events/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/joaovds/learn-events/pkg/events"
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+
 	event := events.NewEvent("hello", "Hello, World!")
 
 	eventHandler := events.NewEventHandler(1)
